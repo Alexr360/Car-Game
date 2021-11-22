caefd46264b43bb945ba6330313b8cd33606e309
Cora2 <44071549+Cora2@users.noreply.github.com>
2021-11-22 12:52:21 -0700

    - Updated

--- Assets/Stuffs/Scripts/playerMove.cs
+++ Assets/Stuffs/Scripts/playerMove.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class playerMove : MonoBehaviour
 {
+    public float movementSpeed = 10;
     void Update()
     {
-        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * 10, 0, Input.GetAxis("Vertical") * Time.deltaTime * 10);
-        //Shift + W/S/A/D to move
-        
-        
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed, Input.GetAxis("Vertical") * Time.deltaTime * movementSpeed / 4, 0);
     }
 }
